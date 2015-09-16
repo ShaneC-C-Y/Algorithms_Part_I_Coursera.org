b1ddfe157175aefc70c406131ed6c3a1c315a6a4
Shane-C-C-Y <b99611040@ntu.edu.tw>
2015-09-16 00:10:52 -0700

    finish add and remove

--- Randomized Queues and Deques/Deque.java
+++ Randomized Queues and Deques/Deque.java
@@ -6,17 +6,18 @@ import java.util.NoSuchElementException;
 public class Deque<Item> implements Iterable<Item> {
     private int N;
     private Node first;
-    private Node end;
+    //private Node end;
     
     private class Node {
         private Item item;
         private Node next;
+        private Node prev;
     }
     
     public Deque() {
         N = 0;
         first = null;
-        end = null;
+        //end = null;
         assert check();
     }// construct an empty deque
     
@@ -29,32 +30,56 @@ public class Deque<Item> implements Iterable<Item> {
     }// return the number of items on the deque
     
     public void addFirst(Item item) {
-        Node temp = first;  // Item temp = first.item (X
+        Node old_first = first;
         first = new Node();
+        if (N == 0) {
+            first.next = first;
+            first.prev = first;
+        }
+        else {
+            first.next = old_first;
+            first.prev = old_first.prev;
+            first.next.prev = first;
+            first.prev.next = first;
+        }
         first.item = item;
-        first.next = temp;
         N++;
-        if (N == 1)
-            end = first;
         assert check();
     }          // add the item to the front
     public void addLast(Item item) {
-        Node temp = end;
-        end = new Node();
-        temp.next = end;
-        end.item = item;
+        Node old_end = first.prev;
+        first.prev = new Node();
+        if (N == 0) {
+            first.next = first;
+            first.prev = first;
+        }
+        else {
+            first.prev.next = first;
+            first.prev.prev = old_end;
+            old_end.next = first.prev;
+        }
+        first.prev.item = item;
         N++;
-        if (N == 1)
-            first = end;
         assert check();
     }          // add the item to the end
     public Item removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException("no element");
         Item item = first.item;
+        first.prev = first.next;
+        first.next = first.prev;
         first = first.next;
+        N--;
+        assert check();        
         return item;
     }               // remove and return the item from the front
     public Item removeLast() {
-        return end.item;
+        if (isEmpty()) throw new NoSuchElementException("no element");
+        Item item = first.prev.item;
+        first.prev.next = first;
+        first.prev = first.prev.prev;
+        N--;
+        assert check();
+        return item;
     }                // remove and return the item from the end
     public Iterator<Item> iterator() {
         List<Item> dequelist = new ArrayList<Item>();
@@ -63,7 +88,7 @@ public class Deque<Item> implements Iterable<Item> {
     }        // return an iterator over items in order from front to end
     private boolean check() {
         int countN = 0;
-        for (Node x = first; x != end; x = x.next)
+        for (Node x = first; x != first.prev; x = x.next)
             countN++;
         return countN++ == N;
     }
